f4817209739a3b369f538c710cde33322751cc6b
Sowmyashr1 <sowmyachinna1@gmail.com>
2025-08-14 18:17:36 +0530

    Update ToCreateContactTest.java

--- DummyProject/src/test/java/ContactTest/ToCreateContactTest.java
+++ DummyProject/src/test/java/ContactTest/ToCreateContactTest.java
@@ -3,7 +3,8 @@ package ContactTest;
 public class ToCreateContactTest {
 
 	public static void main(String[] args) {
-		System.out.println("VontactTest Created Successfully");
+		System.out.println("ContactTest Created Successfully");
 	}
 
 }
+
