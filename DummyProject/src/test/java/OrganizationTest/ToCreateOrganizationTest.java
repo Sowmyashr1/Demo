f3c708bad9b86f382c9ca3283406140cffbface2
Sowmyashr1 <sowmyachinna1@gmail.com>
2025-08-14 18:18:32 +0530

    Update ToCreateOrganizationTest.java

--- DummyProject/src/test/java/OrganizationTest/ToCreateOrganizationTest.java
+++ DummyProject/src/test/java/OrganizationTest/ToCreateOrganizationTest.java
@@ -3,7 +3,8 @@ package OrganizationTest;
 public class ToCreateOrganizationTest {
 
 	public static void main(String[] args) {
-		System.out.println("");
+		System.out.println("OrganizationTest created Successfully");
 	}
 
 }
+
